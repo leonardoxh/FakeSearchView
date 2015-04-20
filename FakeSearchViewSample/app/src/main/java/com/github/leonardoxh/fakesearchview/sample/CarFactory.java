6c5dca7d82426c12ead63f6c79b860d7dab5112e
Leonardo Rossetto <leonardoxh@gmail.com>
2015-04-20 11:10:23 -0700

    modify sample

--- FakeSearchViewSample/app/src/main/java/com/github/leonardoxh/fakesearchview/sample/CarFactory.java
+++ FakeSearchViewSample/app/src/main/java/com/github/leonardoxh/fakesearchview/sample/CarFactory.java
@@ -1,20 +1,20 @@
 package com.github.leonardoxh.fakesearchview.sample;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 final class CarFactory {
 
   static List<Car> getCars() {
-    return Arrays.asList(
-        new Car("Ferrari"),
-        new Car("Lamborghini"),
-        new Car("Bugati"),
-        new Car("Wolksvagen"),
-        new Car("Fiat"),
-        new Car("Renault"),
-        new Car("Mercedes")
-    );
+    List<Car> cars = new ArrayList<>();
+    cars.add(new Car("Ferrari"));
+    cars.add(new Car("Lamborghini"));
+    cars.add(new Car("Bugati"));
+    cars.add(new Car("Wolksvagen"));
+    cars.add(new Car("Fiat"));
+    cars.add(new Car("Renault"));
+    cars.add(new Car("Mercedes"));
+    return cars;
   }
 
 }
